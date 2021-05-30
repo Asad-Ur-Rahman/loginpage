b7c39d2f1e0b7b9e2f0b142d125063dd51f7ae0a
Asad Ur Rahman <AsadRahman511@gmail.com>
2021-05-30 19:30:38 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("Login Page")),),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+
+              Container(
+                width: 200,
+                child: TextField(),
+              ),
+
+              ElevatedButton(onPressed: (){}, child: Text("Login"))
+               
+            ],
+          
+          ),
+        ),
+        ),
+      
+    );
+  }
+}
