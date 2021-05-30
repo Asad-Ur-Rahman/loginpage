7da2b1f4e4b0ce637a10b2b5ebe8d88ebf4d8558
Asad Ur Rahman <AsadRahman511@gmail.com>
2021-05-30 19:47:01 +0500

    Update main.dart
    spacing added

--- lib/main.dart
+++ lib/main.dart
@@ -8,26 +8,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Login Page")),),
         body: Center(
           child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
+              SizedBox(height: 80),
               Container(
+          
                 width: 200,
                 child: TextField(decoration: InputDecoration(
-                  hintText: 'User name'
+                  hintText: 'username'
                   ),),
               ),
 
               Container(
                 width: 200,
                 child: TextField(decoration: InputDecoration(
-                  hintText: 'Password'
+                  hintText: 'password'
                   ),),
               ),
 
+
+              SizedBox(height: 40,),
               ElevatedButton(onPressed: (){}, child: Text("Login"))
                
             ],
